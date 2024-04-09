33e2cb93cd17c781339f4b2db13c8670a7495a8c
Amy Matthews <amy.matthews@thenational.academy>
2024-04-09 18:34:31 +0100

    feat: allow the repo description to be printed

--- python/main.py
+++ python/main.py
@@ -1,8 +1,17 @@
 from github import Github
 # Docs = https://pygithub.readthedocs.io/en/stable/introduction.html
+# Repo = https://github.com/PyGithub/PyGithub
+
+github_api = Github()
+
+def get_repo_description(owner, repo_name):
+    repo = github_api.get_repo('{}/{}'.format(owner, repo_name))
+    return repo.description
 
 def main():
-    github_api = Github()
+    # Uncomment the below to print the description of a repo
+    # print(get_repo_description('oaknational', 'oak-web-application'))
+    pass
 
 
 if __name__ == '__main__':
