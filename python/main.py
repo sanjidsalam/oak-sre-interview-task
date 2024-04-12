4ee6403eab5cd47a9e4f7b84791ab73d6853408a
sanjidsalam <140099751+sanjidsalam@users.noreply.github.com>
2024-04-12 11:31:22 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
 from github import Github
+github_api=Github()
 
 # Docs = https://pygithub.readthedocs.io/en/stable/introduction.html
 # Repo = https://github.com/PyGithub/PyGithub
@@ -6,7 +7,12 @@ from github import Github
 def get_repo_description(github_api, owner, repo_name):
     """ Get the description of a GitHub repo """
     repo = github_api.get_repo('{}/{}'.format(owner, repo_name))
-    return repo.description
+    issue_open= repo.get_issues(state='open')
+    issue_closed= repo.get_issues(state='closed')
+    return {'open': list(issue_open) , 'closed': list(issue_closed)}
+issue= get_repo_description(github_api, 'oaknational', 'oak-web-application')
+print(issue)
+    #return repo.description
 
 def main():
     """ Main function to run the program """
@@ -15,6 +21,9 @@ def main():
     github_api = Github(seconds_between_requests=0.0, per_page=100)
     print(get_repo_description(github_api, 'oaknational', 'oak-web-application'))
     pass
+
+
+
 
 
 if __name__ == '__main__':
